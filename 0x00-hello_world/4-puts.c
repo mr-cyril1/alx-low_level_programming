dac7eaeb428552c0d44607e43704bdddff43c211
mr-cyril1 <cyrildaniel93@gmail.com>
2024-01-29 09:34:17 +0100

    C programs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: after execusion returns the code back to 0 in a NL
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main is the first call after header
+ *
+ *  Return: Always returns the entire code to 0 
+ */
+
+int main(void)
+{
+ printf("with proper grammer,but the outcome is a piece of art,");
+ return(0);   
+}
+
